b9f1130c328a155442d1e2f74049b51795700694
jsalazarSL <Jonathan.Salazar@starlightsoftwaresolutions.com>
2023-11-29 09:01:39 -0600

    adding img slider to home page

--- app/src/main/java/com/example/costaricatravel/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/costaricatravel/ui/home/HomeFragment.java
@@ -8,8 +8,13 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.costaricatravel.databinding.FragmentHomeBinding;
 import com.example.costaricatravel.utils.AdsController;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
@@ -20,6 +25,18 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        ArrayList imageList = new ArrayList<SlideModel>(); // Create image list
+
+// imageList.add(SlideModel("String Url" or R.drawable)
+// imageList.add(SlideModel("String Url" or R.drawable, "title") You can add title
+
+        imageList.add(new SlideModel("https://res.cloudinary.com/dsfgjdvgt/image/upload/v1701269223/d4wfngonataon7iyqm4c.jpg", "The animal population decreased by 58 percent in 42 years.", ScaleTypes.FIT));
+        imageList.add(new SlideModel("https://bit.ly/2BteuF2", "Elephants and tigers may become extinct.", ScaleTypes.FIT));
+        imageList.add(new SlideModel("https://bit.ly/3fLJf72", "And people do that.", ScaleTypes.FIT));
+
+        ImageSlider imageSlider = binding.imageSlider;
+        imageSlider.setImageList(imageList);
 
 
         AdsController.displayBanner(binding.adView);
