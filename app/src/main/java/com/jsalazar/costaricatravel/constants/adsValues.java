20e309d9837d2bdd205628617cfbba18dbadf55f
jsalazarSL <Jonathan.Salazar@starlightsoftwaresolutions.com>
2024-01-30 12:16:57 -0600

    version 1.1.4

--- app/src/main/java/com/jsalazar/costaricatravel/constants/adsValues.java
+++ app/src/main/java/com/jsalazar/costaricatravel/constants/adsValues.java
@@ -1,7 +1,7 @@
 package com.jsalazar.costaricatravel.constants;
 
 public class adsValues {
-    public static int ADS_PERCENTAGE = 25;
+    public static int ADS_PERCENTAGE = 30;
 
     public static boolean PREMIUM_APP = false;
 }
